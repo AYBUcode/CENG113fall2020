daaa9d1e0e0469635e3fec647b4336897bd2daf9
AYBUcode <38549054+AYBUcode@users.noreply.github.com>
2020-11-06 10:31:59 +0300

    Update Week4p.py

--- Week4p.py
+++ Week4p.py
@@ -1,4 +1,20 @@
-b ={
+var = 100
+if var < 200:
+   print "Expression value is less than 200"
+   if var == 150:
+      print "Which is 150"
+   elif var == 100:
+      print "Which is 100"
+   elif var == 50:
+      print "Which is 50"
+   elif var < 50:
+      print "Expression value is less than 50"
+else:
+   print "Could not find true expression"
+
+print "Good bye!"
+
+'''b ={
     'a' : 122,
     'b' : 123,
     'c' : 124,
@@ -9,4 +25,4 @@ b ={
 inp = input('input a character : ')
 # -1 is the default value if there is no keys that matches the input
 val = b.get(inp, -1)
-print('The result for inp is : ', val)
+print('The result for inp is : ', val)'''
